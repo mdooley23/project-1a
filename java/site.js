e53059ef57673455abf16d68562bc3253f25c018
mdooley23 <mdooley2@hawk.iit.edu>
2021-04-14 00:30:09 -0500

    fixed java script to pull the info from inputs

--- java/site.js
+++ java/site.js
@@ -1,18 +1,18 @@
-jQuery(function($) {
-    $('html').removeClass('nojs');
-    $('html').addClass('hasjs');
-  });
- 
-  function allInputs() {
-    var a = document.forms["form-one"]["first-name"].value;
-    var b = document.forms["form-one"]["last-name"].value;
-    var c = document.forms["form-one"]["email"].value;
-    var d = document.forms["form-one"]["phone"].value;
-    var e = document.forms["form-one"]["dob"].value;
-    if ((a == "") || (b == "") || (c == "") || (d == "") || (e == "")){
+function allInputs() {
+    var a = document.getElementById("full_name");
+    var c = document.getElementById("email");
+    var d = document.getElementById("phone");
+    var e = document.getElementById("birthday");
+
+    if ((a == "") || (c == "") || (d == "") || (e == "")){
       alert("Make sure all sections are properly filled out.");
     }
     else{
       alert("Congradulations you been considered to join our ranks");
     }
+    console.log(a)
+    console.log(b)
+    console.log(c)
+    console.log(d)
+    console.log(e)
   }
